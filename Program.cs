e5ec5b45dd5c8114150c54b061b83547fe65b960
Sue-Ting Yeh <suetingyeh@gmail.com>
2019-12-10 20:51:40 +0800

    reversed string

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+static class ReverseString{
+    public static string Reverse( string s )
+ {
+    char[] charArray = s.ToCharArray();
+    Array.Reverse( charArray );
+    return new string( charArray );
+ }
+} 
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString.Reverse("framework"));
+            Console.WriteLine(ReverseString.Reverse("test"));
+            Console.WriteLine(ReverseString.Reverse("c sharp"));
+        }
+    }
+}
+
